e7705baf05018fc339030e7a5611ffdd91041c8a
Crowdbotics <team@crowdbotics.com>
2020-11-28 04:14:51 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/upfood_admin_23027/MainActivity.java
@@ -0,0 +1,15 @@
+package com.upfood_admin_23027;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "upfood_admin_23027";
+    }
+}
